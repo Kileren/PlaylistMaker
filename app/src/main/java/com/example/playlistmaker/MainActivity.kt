8d83abe75ee5706fc72d60fcf59e57cba4ae6511
s.bogachev <kileren@yandex.ru>
2023-08-24 21:12:12 +0300

    Sprint #10 Homework

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import com.example.playlistmaker.search.SearchActivity
 
 class MainActivity : AppCompatActivity() {
 
