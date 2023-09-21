052de54fefb8a9cae1b518579317c989b0827664
s.bogachev <kileren@yandex.ru>
2023-09-21 22:50:03 +0300

    History added

--- app/src/main/java/com/example/playlistmaker/models/Track.kt
+++ app/src/main/java/com/example/playlistmaker/models/Track.kt
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 data class Track(
+    val trackId: String, // ID трека
     val trackName: String, // Название композиции
     val artistName: String, // Имя исполнителя
     val artworkUrl100: String?, // Ссылка на изображение обложки
--- app/src/main/java/com/example/playlistmaker/storage/SharedPreferencesStorage.kt
+++ app/src/main/java/com/example/playlistmaker/storage/SharedPreferencesStorage.kt
@@ -2,6 +2,8 @@ package com.example.playlistmaker.storage
 
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
+import com.example.playlistmaker.models.Track
+import com.google.gson.Gson
 
 class SharedPreferencesStorage(private val context: Context) {
 
@@ -9,10 +11,14 @@ class SharedPreferencesStorage(private val context: Context) {
         const val storageKey = "com.practicum.playlistMaker.sharedPreferencesStorage"
 
         const val darkThemeKey = "darkThemeKey"
+        const val searchHistoryKey = "searchHistoryKey"
     }
 
     private val prefs = context.getSharedPreferences(storageKey, MODE_PRIVATE)
 
+    /**
+     * Flag indicates whether dark theme enabled or not
+     */
     var darkTheme: Boolean
         get() {
             return prefs.getBoolean(darkThemeKey, false)
@@ -20,6 +26,21 @@ class SharedPreferencesStorage(private val context: Context) {
         set(value) {
             prefs.edit()
                 .putBoolean(darkThemeKey, value)
+                .apply()
+        }
+
+    /**
+     * Tracks that user searched lately
+     */
+    var searchHistory: Array<Track>
+        get() {
+            val json = prefs.getString(searchHistoryKey, null) ?: return emptyArray()
+            return Gson().fromJson(json, Array<Track>::class.java)
+        }
+        set(value) {
+            val json = Gson().toJson(value)
+            prefs.edit()
+                .putString(searchHistoryKey, json)
                 .apply()
         }
 }
