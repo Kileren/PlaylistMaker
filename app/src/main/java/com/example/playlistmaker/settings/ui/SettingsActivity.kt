d28f63656daae3a456b4896f92c75b00029e3933
s.bogachev <kileren@yandex.ru>
2023-12-09 16:57:48 +0300

    Settings + Binding

--- app/src/main/java/com/example/playlistmaker/settings/ui/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/settings/ui/SettingsActivity.kt
@@ -1,28 +1,23 @@
 package com.example.playlistmaker.settings.ui
 
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
-import com.example.playlistmaker.R
 import com.example.playlistmaker.creator.Creator
-import com.google.android.material.switchmaterial.SwitchMaterial
+import com.example.playlistmaker.databinding.ActivitySettingsBinding
 
 class SettingsActivity: ComponentActivity() {
 
     private lateinit var viewModel: SettingsViewModel
-
-    private val backButton: ImageView by lazy { findViewById(R.id.back_button) }
-    private val themeSwitcher: SwitchMaterial by lazy { findViewById(R.id.theme_switcher) }
-    private val shareAppButton: View by lazy { findViewById(R.id.share_app_button) }
-    private val writeToSupportButton: View by lazy { findViewById(R.id.write_to_support_button) }
-    private val userAgreementButton: View by lazy { findViewById(R.id.user_agreement_button) }
+    private lateinit var binding: ActivitySettingsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         viewModel = ViewModelProvider(
             this,
             SettingsViewModel.getViewModelFactory(
@@ -30,22 +25,23 @@ class SettingsActivity: ComponentActivity() {
                 Creator.createSharingInteractor(this)
             )
         ).get()
+
         setObservers()
         setClickListeners()
     }
 
     private fun setObservers() {
         viewModel.getDarkThemeState().observe(this) {
-            themeSwitcher.isChecked = it
+            binding.themeSwitcher.isChecked = it
         }
     }
 
     private fun setClickListeners() {
-        backButton.setOnClickListener { finish() }
-        shareAppButton.setOnClickListener { viewModel.shareApp() }
-        writeToSupportButton.setOnClickListener { viewModel.writeToSupport() }
-        userAgreementButton.setOnClickListener { viewModel.openUserAgreement() }
-        themeSwitcher.setOnCheckedChangeListener { _, checked ->
+        binding.backButton.setOnClickListener { finish() }
+        binding.shareAppButton.setOnClickListener { viewModel.shareApp() }
+        binding.writeToSupportButton.setOnClickListener { viewModel.writeToSupport() }
+        binding.userAgreementButton.setOnClickListener { viewModel.openUserAgreement() }
+        binding.themeSwitcher.setOnCheckedChangeListener { _, checked ->
             viewModel.switchDarkTheme(checked)
         }
     }
