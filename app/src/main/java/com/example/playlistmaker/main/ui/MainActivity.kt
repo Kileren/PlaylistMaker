eea5c44934f6aff26b19e80337f4cd681f8c5b13
s.bogachev <kileren@yandex.ru>
2023-12-10 13:44:14 +0300

    MediaLibrary moved to separate package

--- app/src/main/java/com/example/playlistmaker/main/ui/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/main/ui/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.playlistmaker.main.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.playlistmaker.MediaLibraryActivity
+import com.example.playlistmaker.library.ui.MediaLibraryActivity
 import com.example.playlistmaker.databinding.ActivityMainBinding
 import com.example.playlistmaker.search.SearchActivity
 import com.example.playlistmaker.settings.ui.SettingsActivity
