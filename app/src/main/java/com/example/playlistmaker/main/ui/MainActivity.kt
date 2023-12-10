bfee42a9e2440aa1923ff601be746497b68f4a95
s.bogachev <kileren@yandex.ru>
2023-12-10 13:42:11 +0300

    Main Activity moved to separate package + binding

---
+++ app/src/main/java/com/example/playlistmaker/main/ui/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.playlistmaker.main.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.playlistmaker.MediaLibraryActivity
+import com.example.playlistmaker.databinding.ActivityMainBinding
+import com.example.playlistmaker.search.SearchActivity
+import com.example.playlistmaker.settings.ui.SettingsActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        binding.searchButton.setOnClickListener {
+            val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+        binding.mediaLibraryButton.setOnClickListener {
+            val mediaLibraryIntent = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(mediaLibraryIntent)
+        }
+        binding.settingsButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+    }
+}
